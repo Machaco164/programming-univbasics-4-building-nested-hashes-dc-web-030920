4fced809aca89cbdd62c74dc5dba874cc57f839d
Raul A Rozo <raul.a.rozo.o@gmail.com>
2020-01-26 14:27:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,10 +27,10 @@ def monopoly_with_third_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :rent_in_dollars => {
-	      :key1 => {},
-	      :key2 => {},
-	      :key3 => {},
-	      :key4 => {}
+	      :one_piece_owned => 25,
+	      :one_piece_owned => {},
+	      :one_piece_owned => {},
+	      :one_piece_owned => {}
 	    },
 	    :names => {
 	      :key1 => {},
