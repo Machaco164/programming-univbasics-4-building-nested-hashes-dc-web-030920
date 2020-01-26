e42969847d0b420819bd9fcb29cea5ee05f1ca76
Raul A Rozo <raul.a.rozo.o@gmail.com>
2020-01-26 14:46:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -60,7 +60,7 @@ def monopoly_with_fourth_tier
 	      :reading_railroad => {
 	        "mortgage_value" => "$100"
 	      },
-	      :pennsylvania_railroad => {},
+	      "pennsylvania_railroad" => $200,
 	      :b_and_o_railroad => {},
 	      :shortline_railroad => {}
 	    }
